1fb76c4242bb14ce90450d78cd3f5f9cf124daf1
Tuomas Jomppanen <tuomas@jomppanen.com>
2016-06-04 13:37:52 +0300

    landing page and redirect registered user to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,13 @@ Rails.application.routes.draw do
 
   resources :translations, only: [:index, :new, :create, :show]
 
-  root to: "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'dashboard#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
