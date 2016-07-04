61efce4c95c92a2bcbb2b83c40ea266b7705a0ad
Tuomas Jomppanen <tuomas@jomppanen.com>
2016-07-04 23:32:25 +0300

    improving the code, making partials etc

--- app/controllers/word_descriptions_controller.rb
+++ app/controllers/word_descriptions_controller.rb
@@ -2,8 +2,24 @@ class WordDescriptionsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    word_id = params[:word_id]
-    @word = Word.find(word_id)
-    @word_description = @word.descriptions.build
+    @word_description = word.descriptions.build
+  end
+
+  def create
+    if @word_description = word.descriptions.create(word_description_params)
+      redirect_to @word
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def word
+    @word ||= Word.find(params[:word_id])
+  end
+
+  def word_description_params
+    params.require(:word_description).permit(:body, :language_id)
   end
 end
