7745f72fac368e5ec7c93611e3d2619b306e7be7
Tuomas Jomppanen <tuomas@jomppanen.com>
2016-07-04 23:44:58 +0300

    modify word descriptions

--- app/controllers/word_descriptions_controller.rb
+++ app/controllers/word_descriptions_controller.rb
@@ -13,6 +13,20 @@ class WordDescriptionsController < ApplicationController
     end
   end
 
+  def edit
+    @word_description = word.descriptions.find(params[:id])
+  end
+
+  def update
+    @word_description = word.descriptions.find(params[:id])
+    if @word_description.update_attributes(word_description_params)
+      flash[:success] = t('words.word_description.form.successfully_updated')
+      redirect_to @word
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def word
